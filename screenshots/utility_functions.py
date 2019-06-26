4c03125d11e19659ec94c5ab7a16786256851498
David Kooi <kooi.david.w@gmail.com>
2019-06-26 08:40:41 -0700

    Added utility function definitions

---
+++ screenshots/utility_functions.py
@@ -0,0 +1,26 @@
+
+def extract_pacman(image_path):
+    """
+    @image_path: String of path to image file
+    @returns: A tuple of pacman's (x,y) position
+    """
+
+    pass
+
+
+def extract_blue_ghost(image_path):
+    """
+    @image_path: String of path to image file
+    @returns: A tuple of blue ghost's (x,y) position
+    """
+    pass
+
+def extract_pills(image_path):
+    """
+    @image_path: String of path to image file
+    @returns: A list of tuples of pill (x,y) positions
+              I.E: [(x1,y1),(x2,y2),...,(xn,yn)]
+    """
+    pass
+
+
