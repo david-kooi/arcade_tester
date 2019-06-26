e81c81c30d649b55e8e2e85117ce0f3971e67a72
Howard Tang <howardltang@gmail.com>
2019-06-26 12:38:32 -0700

    save changes

--- screenshots/utility_functions.py
+++ screenshots/utility_functions.py
@@ -21,6 +21,9 @@ def extract_pills(image_path):
     @returns: A list of tuples of pill (x,y) positions
               I.E: [(x1,y1),(x2,y2),...,(xn,yn)]
     """
-    pass
+    img = cv2.imread("screenshot_1.png")
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    result = cv2.inRange(img, np.array([5, 28.8, 97.2]), np.array([9, 32.8, 101.2]))
+    cv2. imshow(result, "pills")
 
 
